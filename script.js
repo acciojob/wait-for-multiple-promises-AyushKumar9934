1dc682d33034ffd659d8c270cf74374e5b0121bd
AyushKumar9934 <83179865+AyushKumar9934@users.noreply.github.com>
2024-04-15 21:11:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,48 @@
 //your JS code here. If required.
+const pro1=()=>{
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            resolve(['promise1',1002])
+        },1002)
+    })
+}
+
+const pro2=()=>{
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            resolve(["promise2",2002])
+        },2002)
+    })
+}
+
+
+const pro3=()=>{
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            resolve(["promise3",2052])
+        },2052)
+    })
+}
+let body=document.getElementById("output");
+function solve(){
+    let obj1=new Date();
+    console.log(obj1);
+    Promise.all([pro1(),pro2(),pro3()]).then((data)=>{
+        console.log(data);
+        let obj2=new Date();
+        console.log(obj2);
+        body.innerHTML="";
+        data.forEach(element => {
+            console.log(element);
+            body.innerHTML+=`<tr>
+            <th>${element[0]}</th>
+            <th>${element[1]}</th>
+        </tr>`
+        });
+        body.innerHTML+=` <tr>
+        <th>Total</th>
+        <th>${obj2-obj1}</th>
+    </tr>`
+    })
+}
+solve();
